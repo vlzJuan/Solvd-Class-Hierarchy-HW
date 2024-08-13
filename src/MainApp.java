3e64416399493f6c0e52048a77e855a836871e6a
vlzJuan <valencellojuan@gmail.com>
2024-08-13 12:38:06 -0300

    [MainApp]: -Added code to somewhat show functionality and correct construction of the classes in the 'products' package.

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,2 +1,36 @@
+import products.Inventory;
+import products.Product;
+
+/**
+ * Class used to test all classes (except the abstract ones) can be instantiated.
+ */
 public class MainApp {
+
+
+    public static void main(String[] args){
+
+        ///////////////////////////////////////////////////////////////////////////////
+        //  VERIFICATION OF PACKAGE 'products':
+
+        Product prodA = new Product("asd", 50, 1.0);
+        System.out.println(prodA);
+        System.out.println("\n");
+
+        Inventory storeInventory = new Inventory();
+
+        storeInventory.addProduct(prodA);
+        storeInventory.addProduct(
+                new Product("Water 500ml", 50, 1.6));
+        storeInventory.addProduct(
+                new Product("Orange Juice 500ml", 20, 2.3));
+        storeInventory.addProduct(
+                new Product("Yogurt 500ml", 15, 2.1));
+        System.out.println(storeInventory);
+        //
+        ///////////////////////////////////////////////////////////////////////////////
+
+
+    }
+
+
 }
