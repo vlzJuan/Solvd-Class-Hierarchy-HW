e7103a3fbbabd8017fe91b3824e6f5e09eace6f0
vlzJuan <valencellojuan@gmail.com>
2024-08-13 14:09:31 -0300

    (A)[User]: - Added getters and setters for the User attributes.

--- src/users/User.java
+++ src/users/User.java
@@ -24,4 +24,25 @@ public abstract class User {
 
     // Implement further methods here.
 
+    //  Getters:
+
+    public String getUserName(){
+        return this.userName;
+    }
+
+    public String getPassword(){
+        return this.password;
+    }
+
+    //  Setters:
+
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+
 }
