251eff763d3f6684cc10378f82f141aac54645c4
vlzJuan <valencellojuan@gmail.com>
2024-08-20 14:28:36 -0300

    [PaymentMethod]: -Added the ```int balance``` attribute to this class and its constructor.

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,5 +1,10 @@
+import products.CartProduct;
 import siteutilities.Inventory;
 import products.Product;
+
+import static java.lang.String.format;
+import static java.lang.Thread.sleep;
+import static serviceclasses.MenuPromptsService.promptProduct;
 
 /**
  * Class used to test all classes (except the abstract ones) can be instantiated.
@@ -9,24 +14,13 @@ public class MainApp {
 
     public static void main(String[] args){
 
+        /*
         ///////////////////////////////////////////////////////////////////////////////
         //  VERIFICATION OF PACKAGE 'products':
 
         Product prodA = new Product("Nachos", 50, 1.0);
-        System.out.println(prodA);
-        System.out.println("\n");
-
-        System.out.println("Modifying prodA's stock to 77, and cost to 3.9");
-
-        prodA.setStock(77);
-        prodA.setCost(3.9);
-
-        System.out.println("prodA's stock is:" + prodA.getStock());
-        System.out.println("prodA's cost is: " + prodA.getCost());
-
 
         Inventory storeInventory = new Inventory();
-
         storeInventory.addProduct(prodA);
         storeInventory.addProduct(
                 new Product("Water 500ml", 50, 1.6));
@@ -37,6 +31,25 @@ public class MainApp {
         System.out.println(storeInventory);
         //
         ///////////////////////////////////////////////////////////////////////////////
+        */
+
+        System.out.println("Checking comparisson between two Products:");
+
+        Product prod1 = promptProduct();
+        Product prod2 = promptProduct();
+
+        System.out.println(format("prod1: {%s}, prod2: {%s}",prod1, prod2));
+        System.out.println("Hashcodes are: " + prod1.hashCode() + " for prod1 and "
+                + prod2.hashCode() + " for prod2.");
+        System.out.println("Are both products equal? " + prod1.equals(prod2));
+
+        System.out.println("Instantiating a CartProduct based off prod1");
+        CartProduct cartProduct = new CartProduct(prod1);
+
+        System.out.println("Should be equal to prod1... is it? " + cartProduct.equals(prod1));
+        System.out.println("Is it equal to prod2? : " + cartProduct.equals(prod2));
+
+
 
 
     }
