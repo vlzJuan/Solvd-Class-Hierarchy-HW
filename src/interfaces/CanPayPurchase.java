5f507b808c0c4f998cafbd2179bde2b733567e82
vlzJuan <valencellojuan@gmail.com>
2024-08-27 13:49:11 -0300

    (I)[CanPayPurchase] created. Applied to 'PaymentMethod' and its child classes

---
+++ src/interfaces/CanPayPurchase.java
@@ -0,0 +1,14 @@
+package interfaces;
+
+import siteutilities.Cart;
+
+public interface CanPayPurchase {
+
+    /**
+     * Method used to make this class pay for a purchase.
+     */
+    public boolean payPurchase(Cart cart);
+
+
+
+}
