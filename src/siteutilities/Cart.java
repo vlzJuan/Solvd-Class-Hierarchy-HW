4c11c552b70bd8c63bc7a62d00e518ac1bead0c3
vlzJuan <valencellojuan@gmail.com>
2024-08-27 13:45:38 -0300

    -Method addProduct implemented -Method totalCost implemented -Method MenuDescriptor implemented -Method isRetrievable implemented -Method retrieve TO IMPLEMENT -method size() implemented -method buy(Client) implemented

--- src/siteutilities/Cart.java
+++ src/siteutilities/Cart.java
@@ -1,28 +1,32 @@
 package siteutilities;
 
 
+import interfaces.Purchasable;
+import interfaces.SearchableStorage;
+import products.CartProduct;
 import products.Product;
+import users.Client;
 
 import java.util.ArrayList;
 
 /**
  * Class associated with a Shopping cart for the E-commerce site.
  */
-public class Cart {
+public class Cart implements Purchasable, SearchableStorage<CartProduct> {
 
     //  Attributes:
     //      An arraylist of products. (private)
     //      A boolean purchase state (public)
-    private ArrayList<Product> productList;
+    private ArrayList<CartProduct> productList;
     public boolean stateOfPurchase;
     public boolean stateOfDelivery; //
 
     /**
      * Constructor with parameters for this
      */
-    public Cart(ArrayList<Product> productList){
+    public Cart(){
         //  Create a copy of the list (to avoid issues with references).
-        this.productList = new ArrayList<>(productList);
+        this.productList = new ArrayList<>();
         this.stateOfPurchase = false;
         stateOfDelivery = false;
     }
@@ -37,7 +41,7 @@ public class Cart {
 
         StringBuilder ret = new StringBuilder("Cart products:\n");
 
-        for(Product prod:productList){
+        for(CartProduct prod:productList){
             ret.append(prod.toString()).append("\n");
         }
 
@@ -76,11 +80,65 @@ public class Cart {
     @Override
     public int hashCode(){
         int ret = 0;
-        for(Product prod:this.productList){
+        for(CartProduct prod:this.productList){
             ret = ret + prod.hashCode();
         }
         return ret;
     }
 
 
+
+    public void addProduct(Product prod, int amount){
+        CartProduct aux = new CartProduct(prod, amount);
+        this.productList.add(aux);
+    }
+
+
+    public double totalCost(){
+        double cost = 0.0;
+        for(CartProduct prod:this.productList){
+            cost = cost + prod.totalCost();
+        }
+        return cost;
+    }
+
+
+    @Override
+    public String menuDescriptor() {
+
+        StringBuilder retornable = new StringBuilder();
+        retornable.append("Select the object to choose:\n");
+        for(int i=0; i<productList.size(); i++){
+            retornable.append(i + " - " + productList.get(i).descriptorForMenu() + "\n");
+        }
+        retornable.append("'-1' - Exit the menu.\n");
+        return retornable.toString();
+    }
+
+    @Override
+    public boolean isRetrievable(int index) {
+        return (index>=0 && index<this.size());
+    }
+
+    @Override
+    public CartProduct retrieve(int index) {
+
+        // IMPLEMENT
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return this.productList.size();
+    }
+
+
+    @Override
+    public void buy(Client buyer) {
+        buyer.payPurchase(this);
+    }
 }
+
+
+
+
