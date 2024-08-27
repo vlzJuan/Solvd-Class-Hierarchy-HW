98359d253a280cd761fe0f7a547aa53e13dfc1c5
vlzJuan <valencellojuan@gmail.com>
2024-08-27 13:49:42 -0300

    (I)[IndexableByMenu] created. Applied to 'Product' and 'CartProduct'

---
+++ src/interfaces/IndexableByMenu.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+
+
+public interface IndexableByMenu {
+
+
+    /**
+     *
+     * @return A descriptive string used to show this item in a menu.
+     */
+    public String descriptorForMenu();
+
+
+}
